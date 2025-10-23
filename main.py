51f82129dc70c2870f3a489956f8c1c0050db71d
Crypto-215 <caleb.bull@kingsway.school.nz>
2025-10-24 10:08:57 +1300

    Version 3.1
    Added a function :)

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Version 3
 
 # Making the program more pythonic.
-keep_going = ""
+'''keep_going = ""
 while keep_going == "":
     # Covert answer to lowercase using .lower()
     like_coffee = input("Do you like coffee? ") .lower()
@@ -21,9 +21,40 @@ while keep_going == "":
             print("I am sorry. That is all I have for now.")
 
         else:
-            print("I don't undertsant. Please answer with either Yes or No.")
+            print("I don't undertsand. Please answer with either Yes or No.")
 
     else: # Error Message
         print("I don't understand. Please answer with either Yes or No.")
 
-    keep_going = input("Press <ENTER> to contine, or any other key to quit. Thanks!")
+    keep_going = input("Press <ENTER> to contine, or any other key to quit. Thanks!")'''
+
+# Version 3.1
+def coffee_program():
+    keep_going = ""
+    while keep_going == "":
+        # Covert answer to lowercase using .lower()
+        like_coffee = input("Do you like coffee? ") .lower()
+        if like_coffee == "yes" or like_coffee == "y":
+            print("That's great! I like coffee too!")
+
+        elif like_coffee == "no" or like_coffee == "n":
+            print("You are missing out! Why not give it a try?")
+
+            like_tea = input("Do you like tea instead? ") .upper() # Convert input to uppercase using .upper()
+            if like_tea == "YES" or like_tea == "Y":
+                print("Good for you. Give coffee another try :)")
+
+            elif like_tea == "NO" or like_tea == "N":
+                print("I am sorry. That is all I have for now.")
+
+            else:
+                print("I don't undertsand. Please answer with either Yes or No.")
+
+        else: # Error Message
+            print("I don't understand. Please answer with either Yes or No.")
+
+        keep_going = input("Press <ENTER> to contine, or any other key to quit. Thanks!")
+ 
+# Main Routine
+if __name__ == "__main__":
+    coffee_program()
